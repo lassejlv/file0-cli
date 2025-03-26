fc4b9b3557ada3694bf832ae178dbcccfb110780
lasse <77295879+lassejlv@users.noreply.github.com>
2025-03-26 09:22:48 +0100

    Bump dependencies and update version to 0.1.2; improve file existence checks in upload command

--- src/api.ts
+++ src/api.ts
@@ -11,152 +11,145 @@ import { filesize } from 'filesize'
 const API_ENDPOINT = 'https://file0.io'
 
 interface File {
-	id: string
-	userId: string
-	fileName: string
-	fileSize: number
-	fileRegion: string
-	createdAt: string
+  id: string
+  userId: string
+  fileName: string
+  fileSize: number
+  fileRegion: string
+  createdAt: string
 }
 
 const api = ky.create({
-	hooks: {
-		afterResponse: [
-			async (_request, _options, response) => {
-				if (!response.ok) {
-					const json = (await response.json()) as { error: string; message: string }
-					throw new Error(json.error || json.message || 'An unknown error occurred')
-				}
-			}
-		]
-	}
+  hooks: {
+    afterResponse: [
+      async (_request, _options, response) => {
+        if (!response.ok) {
+          const json = (await response.json()) as { error: string; message: string }
+          throw new Error(json.error || json.message || 'An unknown error occurred')
+        }
+      },
+    ],
+  },
 })
 
 export const Login = async (email: string, password: string) => {
-	try {
-		const response = await api
-			.post(`${API_ENDPOINT}/api/auth/login`, {
-				json: {
-					email,
-					password
-				}
-			})
-			.json<{ data: { token: string } }>()
+  try {
+    const response = await api
+      .post(`${API_ENDPOINT}/api/auth/login`, {
+        json: {
+          email,
+          password,
+        },
+      })
+      .json<{ data: { token: string } }>()
 
-		const token = response.data.token
-		await SetToken(token)
-		consola.success(`Logged in as ${email}`)
-	} catch (error) {
-		consola.error(error)
-	}
+    const token = response.data.token
+    await SetToken(token)
+    consola.success(`Logged in as ${email}`)
+  } catch (error) {
+    consola.error(error)
+  }
 }
 
 export const Me = async () => {
-	try {
-		const token = await GetToken()
-		const response = await api
-			.get(`${API_ENDPOINT}/api/auth/me`, {
-				headers: {
-					Cookie: `session=${token}`
-				}
-			})
-			.json<{ email: string }>()
-		consola.info(`Logged in as ${response.email}`)
-	} catch (error) {
-		consola.error(error)
-	}
+  try {
+    const token = await GetToken()
+    const response = await api
+      .get(`${API_ENDPOINT}/api/auth/me`, {
+        headers: {
+          Cookie: `session=${token}`,
+        },
+      })
+      .json<{ email: string }>()
+    consola.info(`Logged in as ${response.email}`)
+  } catch (error) {
+    consola.error(error)
+  }
 }
 
 export const Logout = async () => {
-	try {
-		const sure = confirm('Are you sure you want to logout?')
-		if (!sure) return
-		const token = await GetToken()
-		await api.delete(`${API_ENDPOINT}/api/auth/logout`, {
-			headers: {
-				Cookie: `session=${token}`
-			}
-		})
+  try {
+    const sure = confirm('Are you sure you want to logout?')
+    if (!sure) return
+    const token = await GetToken()
+    await api.delete(`${API_ENDPOINT}/api/auth/logout`, {
+      headers: {
+        Cookie: `session=${token}`,
+      },
+    })
 
-		await DeleteToken()
-		consola.success('Logged out')
-	} catch (error) {
-		consola.error(error)
-	}
+    await DeleteToken()
+    consola.success('Logged out')
+  } catch (error) {
+    consola.error(error)
+  }
 }
 
 export const Upload = async (file: BunFile) => {
-	try {
-		const token = await GetToken()
-		const start = ora(`Uploading ${file.name}...`).start()
-		const formData = new FormData()
-		formData.append('file', file)
-		const response = await api.post(`${API_ENDPOINT}/upload`, {
-			headers: {
-				Cookie: `session=${token}`
-			},
-			body: formData
-		})
+  try {
+    const token = await GetToken()
 
-		const { url, time } = await response.json<{ url: string; time: string }>()
-		await clipboard.write(url)
-		start.succeed(`Uploaded ${file.name} in ${time} and copied url to clipboard!`)
-	} catch (error) {
-		consola.error(error)
-	}
+    const start = ora(`Uploading ${file.name}...`).start()
+
+    const formData = new FormData()
+    formData.append('file', file)
+
+    const response = await api.post(`${API_ENDPOINT}/upload`, {
+      headers: {
+        Cookie: `session=${token}`,
+      },
+      body: formData,
+    })
+
+    const { url, time } = await response.json<{ url: string; time: string }>()
+    await clipboard.write(url)
+    start.succeed(`Uploaded ${file.name} in ${time} and copied url to clipboard!`)
+  } catch (error) {
+    consola.error(error)
+  }
 }
 
 export const GetUsage = async () => {
-	try {
-		const token = await GetToken()
-		const response = await api
-			.get(`${API_ENDPOINT}/api/files/total-storage-size`, {
-				headers: {
-					Cookie: `session=${token}`
-				}
-			})
-			.json<{ pretty: string; procentageUsed: string }>()
+  try {
+    const token = await GetToken()
+    const response = await api
+      .get(`${API_ENDPOINT}/api/files/total-storage-size`, {
+        headers: {
+          Cookie: `session=${token}`,
+        },
+      })
+      .json<{ pretty: string; procentageUsed: string }>()
 
-		consola.info(`Storage usage: ${response.pretty} (${response.procentageUsed}%)`)
-	} catch (error) {
-		consola.error(error)
-	}
+    consola.info(`Storage usage: ${response.pretty} (${response.procentageUsed}%)`)
+  } catch (error) {
+    consola.error(error)
+  }
 }
 
 export const GetFiles = async () => {
-	const spinner = ora('Fetching files...').start()
+  try {
+    const token = await GetToken()
+    const response = await api
+      .get(`${API_ENDPOINT}/api/files`, {
+        headers: {
+          Cookie: `session=${token}`,
+        },
+      })
+      .json<File[]>()
 
-	try {
-		const token = await GetToken()
-		const response = await api
-			.get(`${API_ENDPOINT}/api/files`, {
-				headers: {
-					Cookie: `session=${token}`
-				}
-			})
-			.json<File[]>()
+    // Create a pretty table for the files
+    const table = new Table({
+      head: [chalk.cyan('Name'), chalk.cyan('Size'), chalk.cyan('Region'), chalk.cyan('Created At')],
+      colWidths: [40, 15, 12, 25],
+    })
 
-		spinner.succeed('Files fetched successfully')
+    response.forEach((file) => {
+      const name = file.fileName.split('/').pop() || file.fileName
+      table.push([chalk.white(name), chalk.yellow(filesize(file.fileSize)), chalk.green(file.fileRegion), chalk.blue(file.createdAt)])
+    })
 
-		// Create a pretty table for the files
-		const table = new Table({
-			head: [chalk.cyan('Name'), chalk.cyan('Size'), chalk.cyan('Region'), chalk.cyan('Created At')],
-			colWidths: [40, 15, 12, 25]
-		})
-
-		response.forEach((file) => {
-			const name = file.fileName.split('/').pop() || file.fileName
-			table.push([
-				chalk.white(name),
-				chalk.yellow(filesize(file.fileSize)),
-				chalk.green(file.fileRegion),
-				chalk.blue(file.createdAt)
-			])
-		})
-
-		console.log(table.toString())
-	} catch (error) {
-		spinner.fail('Failed to fetch files')
-		console.error(chalk.red('Error:'), error)
-	}
+    console.log(table.toString())
+  } catch (error) {
+    consola.error(error)
+  }
 }
--- src/commands/upload.ts
+++ src/commands/upload.ts
@@ -2,18 +2,25 @@ import { Upload } from '@/api'
 import consola from 'consola'
 
 export const UploadCommand = async () => {
-	const path = process.argv[3]
+  const path = process.argv[3]
 
-	if (!path) {
-		return consola.warn('Path is required')
-	}
+  if (!path) {
+    return consola.warn('Path is required')
+  }
 
-	const file = Bun.file(path)
+  const file_exist = await Bun.file(path).exists()
 
-	if (!file.exists) {
-		consola.warn('File does not exist')
-		process.exit(1)
-	}
+  if (!file_exist) {
+    consola.error('File does not exist')
+    process.exit(1)
+  }
 
-	await Upload(file)
+  const file = Bun.file(path)
+
+  if (!file.exists) {
+    consola.warn('File does not exist')
+    process.exit(1)
+  }
+
+  await Upload(file)
 }
